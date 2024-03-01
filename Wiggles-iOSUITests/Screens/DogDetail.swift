47c85c29df2b7220bb973015010c605eb140364d
Isha Soni <soni.isha1@gmail.com>
2024-03-01 14:39:05 +0000

    added smoke tests to validate view dog list and detail

---
+++ Wiggles-iOSUITests/Screens/DogDetail.swift
@@ -0,0 +1,8 @@
+//
+//  DogDetail.swift
+//  Wiggles-iOSUITests
+//
+//  Created by Nehal Soni on 01/03/2024.
+//
+
+import Foundation
---
+++ Wiggles-iOSUITests/Screens/DogList.swift
@@ -0,0 +1,8 @@
+//
+//  DogList.swift
+//  Wiggles-iOSUITests
+//
+//  Created by Nehal Soni on 01/03/2024.
+//
+
+import Foundation
